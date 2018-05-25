d855c29d182f6646b26c68b0c8474d677832d28f
Victor Igumnov <vigumnov@shopzilla.com>
2018-05-25 12:15:24 -0700

    Include the real name for the user list

--- slack-user.c
+++ slack-user.c
@@ -52,8 +52,12 @@ SlackUser *slack_user_update(SlackAccount *sa, json_value *json) {
 		g_hash_table_replace(sa->users, user->object.id, user);
 	}
 
-	const char *name = json_get_prop_strptr(json, "name");
-	g_warn_if_fail(name);
+	const char *nick_name = json_get_prop_strptr(json, "name");
+	g_warn_if_fail(nick_name);
+	const char *real_name = json_get_prop_strptr(json, "real_name");
+	g_warn_if_fail(real_name);
+	char *name = malloc(g_utf8_strlen(nick_name, 512) + g_utf8_strlen(real_name, 512) + 20);
+	sprintf(name, "%s (%s)", nick_name, real_name);
 
 	if (g_strcmp0(user->object.name, name)) {
 		purple_debug_misc("slack", "user %s: %s\n", sid, name);
